117c96b85baad946a75134a3b8df8ae4e6f2516e
David Goldberg <goodwordachemy@gmail.com>
2017-11-03 15:56:50 -0400

    using templates instead of static files directory for serving games

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
 
+MINI_APPS = [
+    {'name': 'Line Game', 'filename': 'line_game.html'},
+    {'name': 'Conway\'s Game Of Life', 'filename': 'conways_game_of_life.html'},
+]
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    print('shart')
-    return render_template('index.html')
+    return render_template('index.html', mini_apps=MINI_APPS)
+
+@app.route('/mini_app/<filename>')
+def mini_app(filename):
+    print('rendering filename: {}'.format(filename))
+    return render_template(filename, mini_apps=MINI_APPS)
 
 if __name__ == '__main__':
     app.run()
